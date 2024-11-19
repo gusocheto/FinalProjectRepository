32ae4d65cd5d3bee29dc0d4a56b0961b86ca8f2e
gusocheto <114013870+gusocheto@users.noreply.github.com>
2024-11-19 23:01:53 +0200

    Html started

--- E-commerceSite.Web.Application/Controllers/HomeController.cs
+++ E-commerceSite.Web.Application/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace E_commerceSite.Web.Application.Controllers
         {
             return View();
         }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ Website.ViewModels/ProductViewModels/ProductViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Website.ViewModels.ProductViewModels
+{
+    public class ProductViewModel
+    {
+        public string ProductName { get; set; } = null!;
+
+        public decimal ProductPrice { get; set; }
+
+        public string? ProductDescription { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+    }
+}
